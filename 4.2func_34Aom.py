6436e26202b958844fb1bf6eec379a271795b9b3
Ornnicha34 <aom.na7452@gmail.com>
2025-01-15 15:17:55 +0700

    Update 4.2func_34Aom.py

---
+++ 4.2func_34Aom.py
@@ -0,0 +1,6 @@
+def Power(Work , time):
+  P = Work / time
+  print("your power is " , ' %.1f ' %P  , "Watt" )
+  #  P คือ พลังงาน , Work คือ งาน , time คือ เวลา , Watt คือ หน่วยของพลังงาน
+
+Power (50 , 5)
